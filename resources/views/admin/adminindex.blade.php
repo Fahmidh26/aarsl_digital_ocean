a03d1a4be0847594b6b2917cc21361a8b17d317a
ifazalam1 <ifaz.alam1@gmail.com>
2023-04-05 13:17:55 +0600

    requisation type ifaz

--- resources/views/admin/adminindex.blade.php
+++ resources/views/admin/adminindex.blade.php
@@ -222,15 +222,14 @@
 			  class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3"
 			>
 			  <h5 class="text-white font-weight-bolder mb-4 pt-2">
-				Work with the rockets
+				A A Rashayan Shilpa Limited
 			  </h5>
 			  <p class="text-white">
-				Wealth creation is an evolutionarily recent positive-sum
-				game. It is all about who take the opportunity first.
+				A A RASHAYAN SHILPA LIMITED is one of the pioneer Suphuric Acid manufacturers in Bangladesh. It prides high quality production and modern chemical technologies.
 			  </p>
 			  <a
 				class="text-white text-sm font-weight-bold mb-0 icon-move-right mt-auto"
-				href="javascript:;"
+				href="https://aarslbd.com"
 			  >
 				Read More
 				<i
